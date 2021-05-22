d38010fb68e53a6801894736ac51ecfdb4c3fae8
Nandan Pandey <nandankpandey@gmail.com>
2021-05-22 15:21:54 +0530

    cocktailDB

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import Home from "./Components/Home";
+import About from "./Components/About";
+import SingleCocktail from "./Components/SingleCocktail";
+import Error from "./Components/Error";
+import Navbar from "./Components/Navigation";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/cocktail/:id">
+          <SingleCocktail />
+        </Route>
+        <Route path="*">
+          <Error />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/Components/About.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function About() {
+    return (
+        <section className="section about-section">
+      <h1 className="section-title">about page</h1>
+      <p>
+      A cocktail is a term relating to any type of alcoholic mixed drink. 
+      Most commonly, cocktails are either a combination of spirits, or one or more spirits mixed 
+      with other ingredients such as fruit juice, flavored syrup, or cream. Cocktails vary widely 
+      across regions of the world, and many websites publish both original recipes and their own 
+      interpretations of older and more famous cocktails.
+      <br />
+      <br />
+      Here it provides some information regarding so many cocktails available in the world
+      </p>
+    </section>
+    )
+}
+
+export default About
